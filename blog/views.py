195fa79be1c50e33e97f9b5434b39b7bda66304c
Salavat <salavatgumerov14@gmail.com>
2024-10-22 22:45:57 +0300

    replaced function post_list to class PostListView

--- blog/views.py
+++ blog/views.py
@@ -1,28 +1,36 @@
 from django.http import Http404
 from django.shortcuts import render, get_object_or_404
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
-
+from django.views.generic import ListView
 
 from .models import Post
 
-def post_list(requset):
-    post_list = Post.published.all()
-    #пагинация с 3 постами на странице
-    paginator = Paginator(post_list, 3)
-    page_number = requset.GET.get('page', 1)
-    try:
-        posts = paginator.page(page_number)
-    except PageNotAnInteger:
-        # Если page_number не целое число, то
-        # выдать первую страницу
-        posts = paginator.page(1)
-    except EmptyPage:
-        # Если page_number находится вне диапазона, то
-        # выдать последнюю страницу
-        posts = paginator.page(paginator.num_pages)
-    return render(requset,
-                  'blog/post/list.html',
-                  {'posts': posts})
+
+class PostListView(ListView):
+    queryset = Post.published.all()
+    context_object_name = 'posts'
+    paginate_by = 3
+    template_name = 'blog/post/list.html'
+
+
+# def post_list(requset):
+#     post_list = Post.published.all()
+#     #пагинация с 3 постами на странице
+#     paginator = Paginator(post_list, 3)
+#     page_number = requset.GET.get('page', 1)
+#     try:
+#         posts = paginator.page(page_number)
+#     except PageNotAnInteger:
+#         # Если page_number не целое число, то
+#         # выдать первую страницу
+#         posts = paginator.page(1)
+#     except EmptyPage:
+#         # Если page_number находится вне диапазона, то
+#         # выдать последнюю страницу
+#         posts = paginator.page(paginator.num_pages)
+#     return render(requset,
+#                   'blog/post/list.html',
+#                   {'posts': posts})
 
 def post_detail(request, year, month, day, post):
     post = get_object_or_404(Post,

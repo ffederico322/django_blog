83823c647e7d1e9e2386f8e846b74df38ff21836
Salavat <salavatgumerov14@gmail.com>
2024-10-22 22:24:11 +0300

    made Paginator, and urls

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,38 @@
-from django.shortcuts import render
+from django.http import Http404
+from django.shortcuts import render, get_object_or_404
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
-# Create your views here.
+
+from .models import Post
+
+def post_list(requset):
+    post_list = Post.published.all()
+    #пагинация с 3 постами на странице
+    paginator = Paginator(post_list, 3)
+    page_number = requset.GET.get('page', 1)
+    try:
+        posts = paginator.page(page_number)
+    except PageNotAnInteger:
+        # Если page_number не целое число, то
+        # выдать первую страницу
+        posts = paginator.page(1)
+    except EmptyPage:
+        # Если page_number находится вне диапазона, то
+        # выдать последнюю страницу
+        posts = paginator.page(paginator.num_pages)
+    return render(requset,
+                  'blog/post/list.html',
+                  {'posts': posts})
+
+def post_detail(request, year, month, day, post):
+    post = get_object_or_404(Post,
+                             status=Post.Status.PUBLISHED,
+                             slug=post,
+                             publish__year=year,
+                             publish__month=month,
+                             publish__day=day)
+
+    return render(request,
+                  'blog/post/detail.html',
+                  {'post': post})
+

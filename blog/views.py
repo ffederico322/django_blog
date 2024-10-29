9f565e0a0555a64de43efb73b9d81346b3ef7350
Salavat <salavatgumerov14@gmail.com>
2024-10-29 16:15:10 +0300

    made the form to send emails

--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,11 @@
+from django.core.mail import send_mail, backends
 from django.http import Http404
 from django.shortcuts import render, get_object_or_404
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.views.generic import ListView
 
 from .models import Post
+from .forms import EmailPostForm
 
 
 class PostListView(ListView):
@@ -44,3 +46,27 @@ def post_detail(request, year, month, day, post):
                   'blog/post/detail.html',
                   {'post': post})
 
+
+def post_share(request, post_id):
+    # Извлечение поста по идентификатору id
+    post = get_object_or_404(Post, id=post_id, status=Post.Status.PUBLISHED)
+    sent = False
+
+    if request.method == 'POST':
+        # Форма передана на обработку
+        form = EmailPostForm(request.POST)
+        if form.is_valid():
+            # Поля формы успешно прошли валидацию
+            cd = form.cleaned_data
+            post_url = request.build_absolute_uri(post.get_absolute_url())
+            subject = f"{cd['name']} recommends you read {post.title}"
+            message = f"Read {post.title} at {post_url}\n\n" \
+                      f"{cd['name']}'s comments: {cd['comments']}"
+            send_mail(subject, message, 'your_email@example.com', [cd['to']])
+            sent = True
+    else:
+        # GET запрос, пустая форма
+        form = EmailPostForm()
+
+    # Возвращаем всегда, чтобы избежать None
+    return render(request, 'blog/post/share.html', {'post': post, 'form': form, 'sent': sent})
